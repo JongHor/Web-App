026ad51abb4fb36db8c5e6f974384b73d26c0728
Thanadon Inthasit <pleum-2013@hotmail.com>
2020-04-10 18:43:04 +0700

    fixed column and add about 10/4/20 18.42

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
 
-export default function About() {
-  return (
-    <div className="parallax">
-      <div className="container mt-5">
-        <div className="card">
-          <img src={'https://firebasestorage.googleapis.com/v0/b/expense-3f50b.appspot.com/o/angkeaw_resize.jpg?alt=media&token=3e1a90ce-7221-4a45-bddf-da7624c587c4'} className='card-img-top' alt="..."  />
-          <div className="card-body">
-            <h5 className="card-title">Developer information</h5>
-            <p>Apichart Nutchanat (610610707)</p>
-            <p>This app use Google Firebase as backend.</p>
-            <Link to="/index">
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <div class="container">
+    <div class="row">
+        <div class="col-sm-2 col-md-2">
+        <img src={'https://firebasestorage.googleapis.com/v0/b/lab8spendmoney.appspot.com/o/profile.jpg?alt=media&token=7a1c3e39-0b1e-41a9-a913-d91249f6f3a2'} className='card-img-top' alt="..."  />
+        </div>
+        <div class="col-sm-2 col-md-4">
+            <blockquote>
+                <p>Thanadon Inthasit(610610584)</p> <small><cite title="Source Title">CPE , CMU</cite></small>
+            </blockquote>
+            <p> <i class="glyphicon glyphicon-envelope"></i><a herf="https://web.facebook.com/pleumXIII">https://web.facebook.com/pleumXIII</a></p>
+        </div>
+        <div class="col-sm-2 col-md-2">
+        <img src={'https://firebasestorage.googleapis.com/v0/b/expense-3f50b.appspot.com/o/angkeaw_resize.jpg?alt=media&token=3e1a90ce-7221-4a45-bddf-da7624c587c4'} className='card-img-top' alt="..."  />
+        </div>
+        <div class="col-sm-2 col-md-4">
+            <blockquote>
+                <p>Apichart Nutchanat (610610707)</p> <small><cite title="Source Title">CPE , CMU</cite></small>
+            </blockquote>
+            <p> <i class="glyphicon glyphicon-envelope"></i><a herf="https://web.facebook.com/peapichart">https://web.facebook.com/peapichart</a></p>
+        </div>
+    </div>
+</div>
+{/* <Link to="/index">
               <h3>
                 <button type="button" className="btn btn-primary">Home</button>
               </h3>
-            </Link>
-          </div>
+</Link> */}
 
-        </div>
-        
       </div>
-      
-    </div>
-  )
+    )
+  }
 }
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -98,18 +98,23 @@ export default class Column extends Component {
         <div className="row">
           <div className="col-md-4 col-md-offset-8 col-sm-6 col-xs-10 col-xs-offset-1 wow animated fadeInLeft">
             <div className="left-text">
-              <h2>.04 Section Four Title</h2>
+            <h2>หอ 3 ชาย (ปรับปรุงใหม่)</h2>
               <div className="line" />
-              <p>Donec suscipit hendrerit euismod. Ut at nisl sem. Praesent ac efficitur mi. Sed fermentum iaculis accumsan. Mauris rutrum dignissim eros id finibus sed turpis in diam mollis lobortis. <br /><br />Phasellus tincidunt leo ante, quis convallis lectus pretium vel. Ut blandit, est eget feugiat laoreet, magna nisi volutpat ligula, commodo efficitur metus magna vel sapien.</p>                  
+              <b><p>ราคาห้องพัก (ต่อ 1 คน)</p></b>
+              <p>- ค่าห้องพักรวม 10,850 บาท *ชำระก่อนเข้าพัก</p>
+              <b><p>ร้านค้าบริเวณหอพัก</p></b>
+              <p>- มินิมาร์ท , ร้านถ่ายเอกสาร , ร้านชานมไข่มุก</p>
+              <b><p>อื่นๆ</p></b>
+              <p>- โรงจอดรถ (เป็นลานจอดรถแบบไม่มีหลังคา)</p>
+              <p>- ห้องคาราโอเกะ</p>
+              <p>- ร้านซักอบรีด</p>
+              <p>- เครื่องซักผ้าหยอดเหรียญ</p>
+              <p>- โรงอาหาร</p>
+              <p>- เครื่องถ่ายเอกสาร Double A Fast Print</p>                  
               <div className="button">
-                <a onClick={this.selectHor}>Read More</a>
+                <a onClick={this.selectHor}>Select</a>
               </div>                  
             </div>
-          </div>
-        </div>
-        <div className="col-md-12">
-          <div className="fourth-arrow">
-            <a href="fake_url" className="scroll-link btn btn-dark" data-id="fifth-section"><i className="fa fa-angle-down" /></a>
           </div>
         </div>
       </div>
@@ -122,19 +127,22 @@ export default class Column extends Component {
         <div className="row">
           <div className="col-md-4 col-sm-6 col-xs-10 wow animated fadeInLeft">
             <div className="left-text">
-              <h2>.03 Section Three Title</h2>
+            <h2>หอ 4 ชาย (ปรับปรุงใหม่)</h2>
               <div className="line" />
-              <p>Please tell your friends about <span className="blue">template</span><span className="green">mo</span>.com website. Ut at nisl sem. Praesent ac efficitur mi. Sed fermentum iaculis accumsan. Mauris rutrum dignissim eros id finibus sed turpis in diam mollis lobortis. <br /><br />Phasellus tincidunt leo ante, quis convallis lectus pretium vel. Ut blandit, est eget feugiat laoreet, magna nisi volutpat ligula, commodo efficitur metus magna vel sapien.</p>
+              <b><p>ราคาห้องพัก (ต่อ 1 คน)</p></b>
+              <p>- ค่าห้องพักรวม 10,850 บาท *ชำระก่อนเข้าพัก</p>
+              <b><p>ร้านค้าบริเวณหอพัก</p></b>
+              <p>- 7-11 , ร้านตัดผม , ตู้ขายเครื่องดื่มอัตโนมัติ</p>
+              <b><p>อื่นๆ</p></b>
+              <p>- โรงจอดรถ (พื้นที่ของมหาวิทยาลัย บริเวณข้างหอพัก)</p>
+              <p>- ห้องคาราโอเกะ</p>
+              <p>- ร้านซักอบรีด</p>
+              <p>- เครื่องซักผ้าหยอดเหรียญ</p>
+              <p>- โรงอาหาร</p>
+              <p>- เครื่องถ่ายเอกสาร Double A Fast Print</p>
               <div className="button">
-                <a >Read More</a>
+                <a >Select</a>
               </div>                  
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 templatemo-position-relative">
-            <div className="fourth-arrow">
-              <a href="fake_url" className="scroll-link btn btn-dark" data-id="fourth-section"><i className="fa fa-angle-down" /></a>
             </div>
           </div>
         </div>
@@ -147,18 +155,22 @@ export default class Column extends Component {
         <div className="row">
           <div className="col-md-4 col-md-offset-8 col-sm-6 col-xs-10 col-xs-offset-1 wow animated fadeInLeft">
             <div className="left-text">
-              <h2>.04 Section Four Title</h2>
+            <h2>หอ 5 ชาย</h2>
               <div className="line" />
-              <p>Donec suscipit hendrerit euismod. Ut at nisl sem. Praesent ac efficitur mi. Sed fermentum iaculis accumsan. Mauris rutrum dignissim eros id finibus sed turpis in diam mollis lobortis. <br /><br />Phasellus tincidunt leo ante, quis convallis lectus pretium vel. Ut blandit, est eget feugiat laoreet, magna nisi volutpat ligula, commodo efficitur metus magna vel sapien.</p>                  
+              <b><p>ราคาห้องพัก (ต่อ 1 คน)</p></b>
+              <p>- ค่าห้องพักรวม 2,400 บาท *ชำระก่อนเข้าพัก</p>
+              <b><p>ร้านค้าบริเวณหอพัก</p></b>
+              <p>- ร้านตัดผม , ตู้ขายเครื่องดื่มอัตโนมัติ</p>
+              <b><p>อื่นๆ</p></b>
+              <p>- โรงจอดรถ</p>
+              <p>- ร้านซักอบรีด</p>
+              <p>- เครื่องซักผ้าหยอดเหรียญ</p>
+              <p>- โรงอาหาร</p>
+              <p>- เครื่องถ่ายเอกสาร Double A Fast Print</p>                  
               <div className="button">
-                <a >Read More</a>
+                <a >Select</a>
               </div>                  
             </div>
-          </div>
-        </div>
-        <div className="col-md-12">
-          <div className="fourth-arrow">
-            <a href="fake_url" className="scroll-link btn btn-dark" data-id="fifth-section"><i className="fa fa-angle-down" /></a>
           </div>
         </div>
       </div>
@@ -170,19 +182,22 @@ export default class Column extends Component {
         <div className="row">
           <div className="col-md-4 col-sm-6 col-xs-10 wow animated fadeInLeft">
             <div className="left-text">
-              <h2>.03 Section Three Title</h2>
+            <h2>หอ 4 หญิง (ปรับปรุงใหม่)</h2>
               <div className="line" />
-              <p>Please tell your friends about <span className="blue">template</span><span className="green">mo</span>.com website. Ut at nisl sem. Praesent ac efficitur mi. Sed fermentum iaculis accumsan. Mauris rutrum dignissim eros id finibus sed turpis in diam mollis lobortis. <br /><br />Phasellus tincidunt leo ante, quis convallis lectus pretium vel. Ut blandit, est eget feugiat laoreet, magna nisi volutpat ligula, commodo efficitur metus magna vel sapien.</p>
+              <b><p>ราคาห้องพัก (ต่อ 1 คน)</p></b>
+              <p>- ค่าห้องพักรวม 10,850 บาท *ชำระก่อนเข้าพัก</p>
+              <b><p>ร้านค้าบริเวณหอพัก</p></b>
+              <p>- ร้านตัดผม , ตู้ขายเครื่องดื่มอัตโนมัติ</p>
+              <b><p>อื่นๆ</p></b>
+              <p>- โรงจอดรถ (เป็นลานจอดรถแบบไม่มีหลังคา)</p>
+              <p>- ห้องคาราโอเกะ</p>
+              <p>- ร้านซักอบรีด</p>
+              <p>- เครื่องซักผ้าหยอดเหรียญ</p>
+              <p>- โรงอาหาร</p>
+              <p>- เครื่องถ่ายเอกสาร Double A Fast Print</p>
               <div className="button">
-                <a >Read More</a>
+                <a >Select</a>
               </div>                  
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 templatemo-position-relative">
-            <div className="fourth-arrow">
-              <a href="fake_url" className="scroll-link btn btn-dark" data-id="fourth-section"><i className="fa fa-angle-down" /></a>
             </div>
           </div>
         </div>
@@ -195,18 +210,23 @@ export default class Column extends Component {
         <div className="row">
           <div className="col-md-4 col-md-offset-8 col-sm-6 col-xs-10 col-xs-offset-1 wow animated fadeInLeft">
             <div className="left-text">
-              <h2>.04 Section Four Title</h2>
+            <h2>หอ 6 หญิง (ปรับปรุงใหม่)</h2>
               <div className="line" />
-              <p>Donec suscipit hendrerit euismod. Ut at nisl sem. Praesent ac efficitur mi. Sed fermentum iaculis accumsan. Mauris rutrum dignissim eros id finibus sed turpis in diam mollis lobortis. <br /><br />Phasellus tincidunt leo ante, quis convallis lectus pretium vel. Ut blandit, est eget feugiat laoreet, magna nisi volutpat ligula, commodo efficitur metus magna vel sapien.</p>                  
+              <b><p>ราคาห้องพัก (ต่อ 1 คน)</p></b>
+              <p>- ค่าห้องพักรวม 10,850 บาท *ชำระก่อนเข้าพัก</p>
+              <b><p>ร้านค้าบริเวณหอพัก</p></b>
+              <p>- ตู้ขายเครื่องดื่มอัตโนมัติ</p>
+              <b><p>อื่นๆ</p></b>
+              <p>- โรงจอดรถ (เป็นลานจอดรถแบบไม่มีหลังคา)</p>
+              <p>- ห้องคาราโอเกะ</p>
+              <p>- ร้านซักอบรีด</p>
+              <p>- เครื่องซักผ้าหยอดเหรียญ</p>
+              <p>- โรงอาหาร</p>
+              <p>- เครื่องถ่ายเอกสาร Double A Fast Print</p>                  
               <div className="button">
-                <a >Read More</a>
+                <a >Select</a>
               </div>                  
             </div>
-          </div>
-        </div>
-        <div className="col-md-12">
-          <div className="fourth-arrow">
-            <a href="fake_url" className="scroll-link btn btn-dark" data-id="fifth-section"><i className="fa fa-angle-down" /></a>
           </div>
         </div>
       </div>
@@ -218,19 +238,21 @@ export default class Column extends Component {
         <div className="row">
           <div className="col-md-4 col-sm-6 col-xs-10 wow animated fadeInLeft">
             <div className="left-text">
-              <h2>.03 Section Three Title</h2>
+            <h2>หอ 40 ปี หญิง</h2>
               <div className="line" />
-              <p>Please tell your friends about <span className="blue">template</span><span className="green">mo</span>.com website. Ut at nisl sem. Praesent ac efficitur mi. Sed fermentum iaculis accumsan. Mauris rutrum dignissim eros id finibus sed turpis in diam mollis lobortis. <br /><br />Phasellus tincidunt leo ante, quis convallis lectus pretium vel. Ut blandit, est eget feugiat laoreet, magna nisi volutpat ligula, commodo efficitur metus magna vel sapien.</p>
+              <b><p>ราคาห้องพัก (ต่อ 1 คน)</p></b>
+              <p>- ค่าห้องพักรวม 7,250 บาท/ภาคเรียน ค่าประกัน 2,000 บาท/ปีการศึกษา *ค่าน้ำ 10 บาท/หน่วย ค่าไฟ 6 บาท/หน่วย (จ่ายวันที่ 1-5 ของเดือน)</p>
+              <b><p>ร้านค้าบริเวณหอพัก</p></b>
+              <p>- ร้านถ่ายเอกสาร , มินิมาร์ท , ห้องฟิตเนส , ที่เรียนดนตรี , ร้านสลัด , ร้านรับซ่อมคอม</p>
+              <b><p>อื่นๆ</p></b>
+              <p>- โรงจอดรถ (เป็นพื้นที่ของมหาวิทยาลัย หน้าหอพัก)</p>
+              <p>- ร้านซักอบรีด</p>
+              <p>- เครื่องซักผ้าหยอดเหรียญ</p>
+              <p>- โรงอาหาร</p>
+              <p>- ตู้เย็น (มีบางห้อง)</p>
               <div className="button">
-                <a >Read More</a>
+                <a >Select</a>
               </div>                  
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 templatemo-position-relative">
-            <div className="fourth-arrow">
-              <a href="fake_url" className="scroll-link btn btn-dark" data-id="fourth-section"><i className="fa fa-angle-down" /></a>
             </div>
           </div>
         </div>
